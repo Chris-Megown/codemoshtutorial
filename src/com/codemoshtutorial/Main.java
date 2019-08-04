0ec18167be1ee67377685d57c7c3ad75ac27a30e
Chris Megown <chris.a.megown@gmail.com>
2019-08-03 22:27:59 -0400

    learning is fun

--- src/com/codemoshtutorial/Main.java
+++ src/com/codemoshtutorial/Main.java
@@ -3,6 +3,8 @@ package com.codemoshtutorial;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int age = 33;
+        System.out.println("Hello");
+        System.out.println("RelaxBeats is the best chill music station");
     }
 }
