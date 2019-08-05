43fbaa40fa653271db3a21ca59eb2f6fb297da59
Chris Megown <chris.a.megown@gmail.com>
2019-08-04 21:15:21 -0400

    moar updates to calc

--- src/com/codemoshtutorial/MortgageCalculator.java
+++ src/com/codemoshtutorial/MortgageCalculator.java
@@ -41,31 +41,6 @@ public class MortgageCalculator {
             System.out.println("Please enter a value between 1 - 30");
         }
         int numberOfPayments = periodInYears * MONTHS_IN_YEAR;
-
-//        do {
-//            System.out.print("Principal Amount ($1,000 - $1,000,000: ");
-//            principal = scanner.nextInt();
-//            if (principal>1000000||principal<1000)
-//                System.out.println("Please enter a value between $1,000 - $1,000,000");
-//        } while (principal>1000000||principal<1000);
-//
-//        do {
-//            System.out.print("Annual Interest Rate: ");
-//            annualInterestRate = scanner.nextFloat();
-//            if (annualInterestRate>12||annualInterestRate<=0)
-//                System.out.println("Please enter a value greater than 0 and less than 12");
-//        } while (annualInterestRate>12||annualInterestRate<=0);
-//
-//
-//
-//        do {
-//            System.out.print("Period (Years): ");
-//            periodInYears = scanner.nextInt();
-//            if (periodInYears>30||periodInYears<1)
-//                System.out.println("Please enter a value between 1 - 30");
-//        } while (periodInYears>30||periodInYears<1);
-
-
         String mortgagePayment = calculateMonthlyMortgagePayment(principal, monthlyInterestRate, numberOfPayments);
         System.out.println("Monthly Mortgage Payment will be: " + mortgagePayment);
     }
