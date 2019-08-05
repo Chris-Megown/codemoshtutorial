5a6229d83527f25a443eb2c155a5cb687b15896c
Chris Megown <chris.a.megown@gmail.com>
2019-08-04 20:37:35 -0400

    loops

--- src/com/codemoshtutorial/Main.java
+++ src/com/codemoshtutorial/Main.java
@@ -1,15 +1,29 @@
 package com.codemoshtutorial;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         helloConsole();
-        new FizzBuzz();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Loops are fun " + i);
+        }
+
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
+
+
     }
 
-    public static void helloConsole(){
+
+    public static void helloConsole() {
         System.out.println("Hello");
     }
 }
