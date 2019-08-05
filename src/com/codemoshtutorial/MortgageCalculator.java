5617a649d192e001fa8fbf7d9e040e9019979a0b
Chris Megown <chris.a.megown@gmail.com>
2019-08-04 21:14:35 -0400

    updated calc

--- src/com/codemoshtutorial/MortgageCalculator.java
+++ src/com/codemoshtutorial/MortgageCalculator.java
@@ -16,30 +16,55 @@ public class MortgageCalculator {
         greeting();
         System.out.println("Please provide some information");
 
-        do {
-            System.out.print("Principal Amount ($1,000 - $1,000,000: ");
+        while(true){
+            System.out.print("Principal Amount ($1,000 - $1,000,000): ");
             principal = scanner.nextInt();
-            if (principal>1000000||principal<1000)
-                System.out.println("Please enter a value between $1,000 - $1,000,000");
-        } while (principal>1000000||principal<1000);
-
-        do {
+            if (principal>=1000&&principal<=1_000_000)
+                break;
+            System.out.println("Please enter a value between $1,000 - $1,000,000");
+        }
+        while(true){
             System.out.print("Annual Interest Rate: ");
             annualInterestRate = scanner.nextFloat();
-            if (annualInterestRate>12||annualInterestRate<=0)
-                System.out.println("Please enter a value greater than 0 and less than 12");
-        } while (annualInterestRate>12||annualInterestRate<=0);
-
+            if (annualInterestRate<=12&&annualInterestRate>0)
+                break;
+            System.out.println("Please enter a value greater than 0 and less than 12");
+        }
         float monthlyInterestRate = (annualInterestRate / PERCENT) / MONTHS_IN_YEAR;
 
-        do {
+        while(true){
             System.out.print("Period (Years): ");
             periodInYears = scanner.nextInt();
-            if (periodInYears>30||periodInYears<1)
-                System.out.println("Please enter a value between 1 - 30");
-        } while (periodInYears>30||periodInYears<1);
-        System.out.print("Period (Years): ");
+            if (periodInYears<=30&&periodInYears>=1) {
+                break;
+            }
+            System.out.println("Please enter a value between 1 - 30");
+        }
         int numberOfPayments = periodInYears * MONTHS_IN_YEAR;
+
+//        do {
+//            System.out.print("Principal Amount ($1,000 - $1,000,000: ");
+//            principal = scanner.nextInt();
+//            if (principal>1000000||principal<1000)
+//                System.out.println("Please enter a value between $1,000 - $1,000,000");
+//        } while (principal>1000000||principal<1000);
+//
+//        do {
+//            System.out.print("Annual Interest Rate: ");
+//            annualInterestRate = scanner.nextFloat();
+//            if (annualInterestRate>12||annualInterestRate<=0)
+//                System.out.println("Please enter a value greater than 0 and less than 12");
+//        } while (annualInterestRate>12||annualInterestRate<=0);
+//
+//
+//
+//        do {
+//            System.out.print("Period (Years): ");
+//            periodInYears = scanner.nextInt();
+//            if (periodInYears>30||periodInYears<1)
+//                System.out.println("Please enter a value between 1 - 30");
+//        } while (periodInYears>30||periodInYears<1);
+
 
         String mortgagePayment = calculateMonthlyMortgagePayment(principal, monthlyInterestRate, numberOfPayments);
         System.out.println("Monthly Mortgage Payment will be: " + mortgagePayment);
